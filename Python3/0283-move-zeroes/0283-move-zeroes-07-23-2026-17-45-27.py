dbc4d311570d4abe4f950cd6554449204f86395b
Anjeet Singh <anjeetsingh7155@gmail.com>
2026-08-12 16:02:19 +0530

    Delete Python3/0283-move-zeroes/0283-move-zeroes-07-23-2026-17-45-27.py

--- Python3/0283-move-zeroes/0283-move-zeroes-07-23-2026-17-45-27.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def moveZeroes(self, nums: List[int]) -> None:
-        move = 0
-        for i in range(len(nums)):
-           if nums[i]!= 0:
-            nums[move] , nums[i] =  nums[i] , nums[move]
-            move+=1
-        
