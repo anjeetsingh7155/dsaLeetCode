d09f48cec75d8f3f0ad6e4d105e45887787876b3
Anjeet Singh <anjeetsingh7155@gmail.com>
2026-08-13 18:27:59 +0530

    Delete Python3/0001-two-sum directory

--- Python3/0001-two-sum/0001-two-sum-08-13-2026-18-27-32.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        seen = dict()
-        for index , value in enumerate(nums):
-            need = target-value
-            if need in seen :return [seen[need],index]
-            seen[value] = index
