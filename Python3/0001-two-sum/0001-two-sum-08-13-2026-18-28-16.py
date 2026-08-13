11eba003a595a92c87cf7a268d36d06e444fb73a
Anjeet Singh <anjeetsingh7155@gmail.com>
2026-08-13 18:28:16 +0530

    time : 3 ms (53.69%) ,space : 20.6 MB (17.49%) , date : 08-13-2026 , problemName: 0001-two-sum, Difficulty : Easy, language : .py , ProblemTopic :Array

---
+++ Python3/0001-two-sum/0001-two-sum-08-13-2026-18-28-16.py
@@ -0,0 +1,7 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        seen = dict()
+        for index , value in enumerate(nums):
+            need = target-value
+            if need in seen :return [seen[need],index]
+            seen[value] = index
